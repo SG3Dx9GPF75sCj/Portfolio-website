f2cdef60dc78a9005c39400c21a4e643d78b6cb9
James Collum <49186603+JimmeyC@users.noreply.github.com>
2022-05-06 14:50:29 +1000

    a

--- redirect.js
+++
@@ -1,3 +0,0 @@
-if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/webOS/i)) || (navigator.userAgent.match(/Blackberry/i)) || (navigator.userAgent.match(/Android/i))){
-    location.replace("mobile/");
-}
